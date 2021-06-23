a53a849f3929e590a39d71e889ee2127131fac2e
Pavan-Saketh-Mandha <84276275+Pavan-Saketh-Mandha@users.noreply.github.com>
2021-06-23 19:16:24 +0530

    Adding line comparision use case 4

--- Line-comparision.java
+++ Line-comparision.java
@@ -1,34 +1,92 @@
-import java.util.*;
-class LengthofLineUC3 {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Line values x1 and y1: ");
-		int x1 = sc.nextInt();
-		int y1 = sc.nextInt();
-		System.out.println("Enter First Line values x2 and y2: ");
-		int x2 = sc.nextInt();
-		int y2 = sc.nextInt();
-		double lengthOfLine1;
-		lengthOfLine1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		System.out.println("Enter Second Line values x3 and y3: ");
-                int x3 = sc.nextInt();
-                int y3 = sc.nextInt();
-                System.out.println("Enter Second Line values x4 and y4: ");
-                int x4 = sc.nextInt();
-                int y4 = sc.nextInt();
-                double lengthOfLine2;
-                lengthOfLine2 = Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
-		String str1 = String.valueOf(lengthOfLine1);
-		String str2 = String.valueOf(lengthOfLine2);
-		
-		System.out.println("The length of the First line is : "+lengthOfLine1);
-		System.out.println("The length of the Second line is : "+lengthOfLine2);
-		
-		if(str1.compareTo(str2) == 0)
-			System.out.println("The length of two lines are equal");
-		else if(str1.compareTo(str2)<0)
-			System.out.println("The length of First line is less than the lenght of Second line");
-		else
-			System.out.println("The lenght of First line is greater than the length of Second line");
-		}
-	}
+/**
+ * Use Java Object Oriented Programming Concepts of Line and Point as well as equals 
+ * and compareTo methods.
+ * - Using Java compareTo method to compare 2
+ * @author Pavan Saketh
+ * @version 1.0
+ * @Since 15-06-2021
+ */
+import java.util.Scanner;
+
+public class LineComparison{
+
+    //main method
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Problem !!!");
+        
+        //Calculating length of first Line by calling lengthOfLine method
+        System.out.println("Enter First Line Details : ");
+        double firstLineLength = lengthOfLine();
+
+        System.out.println();
+
+        //Calculating length of second line by calling lengthOfLine method
+        System.out.println("Enter Second Line Details : ");
+        double secondLineLength = lengthOfLine();
+        
+        equalityOfLines(firstLineLength, secondLineLength);
+        comparisonOfLines(firstLineLength, secondLineLength);
+
+        System.out.println("Length of the first line is : " + firstLineLength);
+        System.out.println("Length of the second line is : " + secondLineLength);
+    }
+    
+    // returns length of line
+    public static double lengthOfLine() {
+
+        System.out.println("Enter the start co-ordinates (x1,y1) :");
+        int x1 = readInt();
+        int y1 = readInt();
+
+        System.out.println("Enter the end co-ordinates (x2,y2) : ");
+        int x2 = readInt();
+        int y2 = readInt();
+
+        return lengthCalculator(x1, y1, x2, y2);
+    }
+
+    // reads user input and returns integer number
+    public static int readInt() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    
+    // Calculates Length of line by applying formula and returns the value
+    public static double lengthCalculator(int x1, int y1, int x2, int y2) {
+
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    }
+    
+    // Checks equality of Two Lines
+    public static void equalityOfLines(double l1, double l2) {
+
+        String line1 = String.valueOf(l1);
+        String line2 = String.valueOf(l2);
+
+        if (line1.equals(line2))
+            System.out.println("The length of both lines is equal");
+
+        else
+            System.out.println("The length of both lines in not equal");
+
+    }
+
+    // Compares Length Of Two Lines
+    public static void comparisonOfLines(double l1, double l2) {
+
+        String line1 = String.valueOf(l1);
+        String line2 = String.valueOf(l2);
+
+        if (line1.compareTo(line2) == 0)
+            System.out.println("Length of Line 1 is equal to Line 2");
+
+        else if (line1.compareTo(line2) < 0)
+            System.out.println("Length of Line 1 is less than Line 2");
+
+        else if (line1.compareTo(line2) > 0)
+            System.out.println("Length of Line 1 is greater than Line 2");
+
+    }
+}
